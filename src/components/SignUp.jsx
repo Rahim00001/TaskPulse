efd8d784684afd353586edb38a7b82a74c01da2e
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-03 19:39:05 +0600

    axios installed and user registerd successfully

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        Axios.post('http://localhost:5000/auth/signup', { username, email, password })
+            .then(response => {
+                console.log(response);
+                navigate('/login')
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
     return (
         <div className='flex justify-center items-center min-h-screen bg-green-50'>
             <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -8,7 +28,7 @@ const SignUp = () => {
                     <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
                     <p className='text-sm text-gray-400'>Welcome to TaskPulse</p>
                 </div>
-                <form className='space-y-6 ng-untouched ng-pristine ng-valid'>
+                <form className='space-y-6 ng-untouched ng-pristine ng-valid' onSubmit={handleSubmit}>
                     <div className='space-y-4'>
                         <div>
                             <label htmlFor='email' className='block mb-2 text-sm'>
@@ -16,7 +36,7 @@ const SignUp = () => {
                             </label>
                             <input type='text' name='name' id='name' placeholder='Enter Your Name Here'
                                 className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-300 bg-gray-200 text-gray-900'
-                            />
+                                onChange={(e) => setUsername(e.target.value)} />
                         </div>
                         <div>
                             <label htmlFor='email' className='block mb-2 text-sm'>
@@ -24,7 +44,7 @@ const SignUp = () => {
                             </label>
                             <input type='email' name='email' id='email' required placeholder='Enter Your Email Here'
                                 className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-300 bg-gray-200 text-gray-900'
-                            />
+                                onChange={(e) => setEmail(e.target.value)} />
                         </div>
                         <div>
                             <div className='flex justify-between'>
@@ -34,7 +54,7 @@ const SignUp = () => {
                             </div>
                             <input type='password' name='password' autoComplete='new-password' id='password' required placeholder='*******'
                                 className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-300 bg-gray-200 text-gray-900'
-                            />
+                                onChange={(e) => setPassword(e.target.value)} />
                         </div>
                     </div>
 
